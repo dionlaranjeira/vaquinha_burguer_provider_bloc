4bc3b0ea086ceb9c0d7feb01a708d3313aab8924
Dion Laranjeira <dionribeiro.rr@gmail.com>
2023-06-25 22:46:57 -0400

    [REFACTOR] Movendo temas para arquivos isolados.

--- lib/dw9_delivery_app.dart
+++ lib/dw9_delivery_app.dart
@@ -1,3 +1,4 @@
+import 'package:app_vaquinha_burguer_provider_bloc/core/ui/theme/theme_config.dart';
 import 'package:app_vaquinha_burguer_provider_bloc/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,7 @@ class Dw9DeliveryApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Delivery App',
+      theme: ThemeConfig.theme,
       routes: {
         '/': (context) => const SplashPage(),
       },
--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -12,42 +12,18 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: ThemeData(
-          scaffoldBackgroundColor: Colors.white,
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.white,
-            elevation: 0,
-            centerTitle: true,
-            iconTheme: IconThemeData(color: Colors.black),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Splash"),
+      ),
+      body: Column(
+        children: [
+          Container(),
+          ElevatedButton(onPressed: () {}, child: const Text("Teste")),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'text'),
           ),
-          primaryColor: ColorsApp.i.primary,
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: ColorsApp.i.primary,
-            primary: ColorsApp.i.primary,
-            secondary: ColorsApp.i.secondary,
-          ),
-          elevatedButtonTheme:
-              ElevatedButtonThemeData(style: AppStyles.i.primaryButton),
-          inputDecorationTheme: InputDecorationTheme(
-              fillColor: Colors.white,
-              filled: true,
-              isDense: true,
-              contentPadding: EdgeInsets.all(16),
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Colors.grey[400]!)))),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Splash"),
-        ),
-        body: Column(
-          children: [
-            Container(),
-            ElevatedButton(onPressed: () {}, child: const Text("Teste")),
-            TextFormField(),
-          ],
-        ),
+        ],
       ),
     );
   }
